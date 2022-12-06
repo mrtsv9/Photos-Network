0ef40283afaaf9402ba2ab867b74268664f3ce0f
MartsevPC <a.martsev85@gmail.com>
2022-12-06 23:22:40 +0300

    initial commit

---
+++ app/src/main/java/com/example/photosnetwork/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.photosnetwork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.photosnetwork.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var toggle: ActionBarDrawerToggle
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.open, R.string.close)
+        binding.drawer.addDrawerListener(toggle)
+        toggle.syncState()
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val headerView = binding.navView.getHeaderView(0)
+        val userName: TextView = headerView.findViewById(R.id.header_username)
+        userName.text = "3423423"
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.container) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        binding.navView.setupWithNavController(navController)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (toggle.onOptionsItemSelected(item)) return true
+        return super.onOptionsItemSelected(item)
+    }
+}
